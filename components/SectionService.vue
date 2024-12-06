d89dacc890b628233dac380c173ccfe4664f2f2a
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2024-12-07 03:40:24 +0500

    update

--- components/SectionService.vue
+++ components/SectionService.vue
@@ -20,7 +20,7 @@
            <div class=" md:flex items-center justify-center gap-[24px] mt-[48px] lg:mt-0 px-[20px] md:px-0 lg:ml-[70px] xl:ml-0">
                 <div class="grid gap-[24px]">
                     <div class="flex items-center gap-[24px] md:gap-0 md:block lg:w-[230px] xl:w-[300px] xl:h-[350px] 2xl:w-[306px]  2xl:h-[360px] bg-[#ECEEF2] hover:bg-[#FF8911] rounded-[16px] shadow-lg p-4 md:p-6 hover:shadow-xl transition-shadow duration-300">
-                        <img src="../assets/images/icons/icon0.png" alt="icon" class="w-[72px] h-[72px] md:w-[72px]">
+                        <img src="/images/icons/icon0.png" alt="icon" class="w-[72px] h-[72px] md:w-[72px]">
                         <div class="md:mt-[70px]">
                             <h3 class="text-[24px] leading-[28px] font-[700]">Network solution</h3>
                             <p class="font-[400] text-[#1A1D27CC] text-[16px] leading-[24px]">Assertively streamline interactive interfaces after value-added infrastructures.</p>
@@ -30,7 +30,7 @@
                         </div>
                     </div>
                     <div class="flex items-center gap-[24px] md:gap-0 md:block lg:w-[230px] xl:w-[300px] xl:h-[350px] 2xl:w-[306px]  2xl:h-[360px] bg-[#ECEEF2] hover:bg-[#FF8911] rounded-[16px] shadow-lg p-4 md:p-6 hover:shadow-xl transition-shadow duration-300">
-                        <img src="../assets/images/icons/icon1.png" alt="icon" class="w-[72px] h-[72px] md:w-[72px]">
+                        <img src="/images/icons/icon1.png" alt="icon" class="w-[72px] h-[72px] md:w-[72px]">
                         <div class="md:mt-[70px]">
                             <h3 class="text-[24px] leading-[28px] font-[700]">Cyber security</h3>
                             <p class="font-[400] text-[#1A1D27CC] text-[16px] leading-[24px]">Assertively streamline interactive interfaces after value-added infrastructures.</p>
@@ -42,7 +42,7 @@
                 </div>
                 <div class="grid gap-[24px] mt-[24px] md:mt-[58px]">
                     <div class="flex items-center gap-[24px] md:gap-0 md:block lg:w-[230px] xl:w-[300px] xl:h-[350px] 2xl:w-[306px]  2xl:h-[360px] bg-[#ECEEF2] hover:bg-[#FF8911] rounded-[16px] shadow-lg p-4 md:p-6 hover:shadow-xl transition-shadow duration-300">
-                        <img src="../assets/images/icons/icon2.png" alt="icon" class="w-[72px] h-[72px] md:w-[72px]">
+                        <img src="/images/icons/icon2.png" alt="icon" class="w-[72px] h-[72px] md:w-[72px]">
                         <div class="md:mt-[70px]">
                             <h3 class="text-[24px] leading-[28px] font-[700]">Data analytics</h3>
                             <p class="font-[400] text-[#1A1D27CC] text-[16px] leading-[24px]">Assertively streamline interactive interfaces after value-added infrastructures.</p>
@@ -52,7 +52,7 @@
                         </div>
                     </div>
                      <div class=" md:gap-0 md:block lg:w-[230px] xl:w-[300px] xl:h-[350px] 2xl:w-[306px] 2xl:h-[360px] bg-[#ECEEF2] hover:bg-[#FF8911] rounded-[16px] shadow-lg md:p-6 hover:shadow-xl transition-shadow duration-300 cursor-pointer">
-                        <img src="../assets/images/icons/arrow.png" alt="icon" class="w-[72px] h-[72px] md:w-[72px] float-right">
+                        <img src="/images/icons/arrow.png" alt="icon" class="w-[72px] h-[72px] md:w-[72px] float-right">
                         <h3 class="text-[24px] leading-[28px] font-[700] mt-[30%] md:mt-[80%] lg:mt-[270px] p-[20px] md:p-0">View all services</h3>
                     </div>
                 </div>
